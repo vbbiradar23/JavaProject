6b7af27a2c46f585404d296b71899e1ea61426e7
vinay <biradar0023@gmail.com>
2022-07-06 11:55:41 +0530

    Adding third demo file javaprg2.java

---
+++ javaprg2.java
@@ -0,0 +1,5 @@
+public class javaprg2{
+public static void main(String[] args){
+System.out.println("This is program is just for demo");
+}
+}
---
+++ javaprg3.java
@@ -0,0 +1,5 @@
+public class Javaprg3{
+public static void main(String[] args){
+System.out.println("This is program is for demo");
+}
+}
